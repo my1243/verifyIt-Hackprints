dc68a522473c72bf02dd9382d4e54240476c88ef
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2023-02-25 14:57:45 +0530

    Home Screen Added

---
+++ client_App/src/components/Home/Navbar.js
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <View>
+      <View className="bg-blue-200 p-2 rounded-xl mt-10">
+        <Text className="text-2xl">Hi Harshil!</Text>
+        <Text className="text-base">Let's Start </Text>
+      </View>
+    </View>
+  );
+};
+
+export default Navbar;
---
+++ client_App/src/components/Home/ScheduleCard.js
@@ -0,0 +1,20 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+
+const ScheduleCard = () => {
+  return (
+    <TouchableOpacity className="my-3">
+      <View className="bg-[#FCEBDC] p-2 rounded-xl border-l-8 border-[#E27A1A] pl-6">
+        <Text className="text-[#E27A1A] font-bold text-lg">AJT Exam</Text>
+        <Text className="text-[#EEAB6D] text-base">Block No. : 12</Text>
+        <View className="flex flex-row items-center space-x-3">
+          <Feather name="clock" size={18} color="#dd8d43" />
+          <Text className="text-[#dd8d43] text-sm ">09.00 - 10.00</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ScheduleCard;
