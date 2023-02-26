c4f18ac4d2da29637a5f031df806bf33042d56b1
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2023-02-26 23:11:26 +0530

    code formatted

--- server/Src/DB/connection.js
+++ server/Src/DB/connection.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb://localhost:27017/Hackprints")
-    .then(() => {
-        console.log(`db connection successfully...`);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+mongoose
+  .connect(
+    "mongodb+srv://harshilsarariya:harshilsarariya@cluster0.ntbnnqv.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log(`db connection successfully...`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- server/Src/Models/faculty-schema.js
+++ server/Src/Models/faculty-schema.js
@@ -1,13 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const FacultySchema = new mongoose.Schema({
-    fId: { type: String, required: true, unique: true },
-    fShortName: { type: String, unique: true, required: true },
-    fName: { type: String, required: true },
-    branch: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "branch" },
-    allotments: [{
-        allotment: { type: mongoose.Schema.Types.ObjectId, ref: "examSchedule" },
-    }],
+  fId: { type: String, required: true, unique: true },
+  fShortName: { type: String, unique: true, required: true },
+  fName: { type: String, required: true },
+  branch: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "branch",
+  },
+  allotments: [
+    {
+      allotment: { type: mongoose.Schema.Types.ObjectId, ref: "examSchedule" },
+    },
+  ],
 });
 
 /*
