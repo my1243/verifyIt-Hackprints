af5c9c73b1f4f6410a03ce874f35861057a7aa05
Harshil Sarariya <harshilsarariya@users.noreply.github.com>
2023-02-27 14:29:38 +0530

    Fields Changed

--- server/Src/Models/student-schema.js
+++ server/Src/Models/student-schema.js
@@ -1,20 +1,25 @@
 const mongoose = require("mongoose");
 
 const StudentSchema = new mongoose.Schema({
-
-    studentId: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    rollNo: { type: String, required: true, unique: true },
-    semester: { type: Number, required: true },
-    clg: { type: String, required: true },
-    img: { type: String },
-    practicalExam: [{
-        subjectCode: { type: String, required: true },
-        examStatus: { type: Boolean, default: false }
-    }],
-    feeStatus: { type: Boolean, default: false },
-    branch: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "branch" },
-    hall: { type: mongoose.Schema.Types.ObjectId, ref: "hall" },
+  studentId: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  rollNo: { type: String, required: true, unique: true },
+  semester: { type: Number, required: true },
+  clg: { type: String, required: true },
+  img: { type: String },
+  practicalExam: [
+    {
+      subjectCode: { type: String, required: true },
+      examStatus: { type: Boolean, default: false },
+    },
+  ],
+  feeStatus: { type: Boolean, default: false },
+  branch: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "branch",
+  },
+  hall: { type: mongoose.Schema.Types.ObjectId, ref: "hall" },
 });
 
 /*
